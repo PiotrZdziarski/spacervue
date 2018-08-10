16bd4d326e0f1227e6c0f6d3e7093acdc4a008d3
Piotr <pzdziarski01@gmail.com>
2018-08-10 10:51:56 +0200

    Fixing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="app">
-    <div class="nav">
-      <router-link to="/">spacer</router-link>
-      <router-link to="/about">about</router-link>
-    </div>
-    <router-view></router-view>
+<div class="app">
+  <div class="nav">
+    <router-link to="/">spacer</router-link>
+    <router-link to="/about">about</router-link>
   </div>
+  <router-view></router-view>
+</div>
 </template>
 
 <style lang="scss" scoped>
